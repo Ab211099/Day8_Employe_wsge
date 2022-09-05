cca66a32623e45f41f1d35dc0b46800d4f384c22
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-05 23:28:22 +0530

    [Abhi]Added:DailyWage

---
+++ Day8_EmpPayRoll/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_EmpPayRoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Empwage program");
+            Console.WriteLine("Choose opion \n 1.Check emp abs or pres \n 2.Calculate daily wage");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1: Class1.EmployeeAttendance(); break;
+                case 2: Class2.CalculateDailyWage(); break;
+            }
+        }
+
+    }
+}
