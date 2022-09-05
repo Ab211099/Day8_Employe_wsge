f34deb634109fac5bfdad0e75e8972d5ecdc5e09
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-05 23:40:53 +0530

    [Abhi]Added:Part time wage

--- Day8_EmpPayRoll/Program.cs
+++ Day8_EmpPayRoll/Program.cs
@@ -5,13 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Empwage program");
-            Console.WriteLine("Choose opion \n 1.Check emp abs or pres \n 2.Calculate daily wage");
+            Console.WriteLine("Choose opion \n 1.Check emp abs or pres \n 2.Calculate daily wage \n 3.Add Part time wage ");
 
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1: Class1.EmployeeAttendance(); break;
                 case 2: Class2.CalculateDailyWage(); break;
+                case 3: Class3.CalculateDailyPartTimeWage(); break;
             }
         }
 
