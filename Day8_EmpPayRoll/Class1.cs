fb2772647160346140fa6e4e614f15fd521cb7c0
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-05 23:15:38 +0530

    [Abhi]Added: Check Emp pres OR Abs

---
+++ Day8_EmpPayRoll/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_EmpPayRoll
+{
+    internal class Class1
+    {
+        public static void EmployeeAttendance()
+        {
+            Random obj = new Random();
+            int num = obj.Next(0, 2);
+            if (num == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
