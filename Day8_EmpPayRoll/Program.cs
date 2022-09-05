4dfcb5b3cf5a49d5d2548ce9def55de733795def
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-05 23:54:47 +0530

    [Abhi]Added:Month empwage

--- Day8_EmpPayRoll/Program.cs
+++ Day8_EmpPayRoll/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Empwage program");
-            Console.WriteLine("Choose opion \n 1.Check emp abs or pres \n 2.Calculate daily wage \n 3.Add Part time wage \n 4.Dailywage usind case ");
+            Console.WriteLine("Choose opion \n 1.Check emp abs or pres \n 2.Calculate daily wage \n 3.Add Part time wage \n 4.Dailywage usind case \n 5.Calculate wage for month");
 
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -14,6 +14,7 @@
                 case 2: Class2.CalculateDailyWage(); break;
                 case 3: Class3.CalculateDailyPartTimeWage(); break;
                 case 4: Class4.CalculateDailyWageUsingCase(); break;
+                case 5: Class5.MonthWage(); break;
             }
         }
 
